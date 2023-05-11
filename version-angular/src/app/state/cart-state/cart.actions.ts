605cdaa344b5c1a72e6542a38b12f611b1c2a3f4
Faris Kantic <faris_kantic@hotmail.com>
2023-05-11 19:54:59 +0200

    completed angular app

--- version-angular/src/app/state/cart-state/cart.actions.ts
+++ version-angular/src/app/state/cart-state/cart.actions.ts
@@ -1,6 +1,13 @@
 import { createAction, props } from '@ngrx/store';
 import { IProduct } from 'src/app/models/app.models';
+import { cart } from 'src/app/storage/storage';
 
-export const addProductToCart = createAction('[Cart] Add product to cart', props<{ product: IProduct, quantity: number }>());
-export const removeProductFromCart = createAction('[Cart] Remove product from cart', props<{ id: number }>());
-export const setProductInCartToSoldOut = createAction('[Cart] Set product in cart to sold out', props<{ id: number }>());
+enum cartActions {
+    addProductToCart = "[Cart] Add product to cart",
+    removeProductFromCart = "[Cart] Remove product from cart",
+    setProductInCartToSoldOut = "[Cart] Set product in cart to sold out"
+};
+
+export const addProductToCart = createAction(cartActions.addProductToCart, props<{ product: IProduct, quantity: number }>());
+export const removeProductFromCart = createAction(cartActions.removeProductFromCart, props<{ id: number }>());
+export const setProductInCartToSoldOut = createAction(cartActions.setProductInCartToSoldOut, props<{ id: number }>());
--- version-angular/src/app/state/products-state/products.actions.ts
+++ version-angular/src/app/state/products-state/products.actions.ts
@@ -1,5 +1,10 @@
 import { createAction, props } from "@ngrx/store";
 import { IProduct } from "src/app/models/app.models";
 
-export const addProduct = createAction('[Products] Add product', props<{ product: IProduct }>());
-export const removeProduct = createAction('[Products] Remove product', props<{ id: number }>());
+enum productActions {
+    addProduct = "[Product] Add product",
+    removeProduct = "[Products] Remove product"
+};
+
+export const addProduct = createAction(productActions.addProduct, props<{ product: IProduct }>());
+export const removeProduct = createAction(productActions.removeProduct, props<{ id: number }>());
